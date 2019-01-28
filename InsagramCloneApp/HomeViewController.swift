aa2dc5185e6572d2b3d3b24767e469c7436eafec
Perez <ebukaperez@gmail.com>
2019-01-28 17:03:41 -0600

    Grabing data from Firebase

--- InsagramCloneApp/HomeViewController.swift
+++ InsagramCloneApp/HomeViewController.swift
@@ -7,20 +7,32 @@
 //
 
 import UIKit
+import FirebaseDatabase
 import FirebaseAuth
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadPosts()
+    }
+    
+    func loadPosts(){
+    let dataBaseRef =   Database.database().reference().child("posts")
+        let childAddedObserver = dataBaseRef.observe(.childAdded) { (snapShot : DataSnapshot) in
+            //grabs all data, included new added child events
+            print(snapShot.value)
+           
+        }
+    }
     @IBAction func logoutButtonPressed(_ sender: Any) {
-       
         do {
      try  Auth.auth().signOut()
         } catch let logOutError {
             NSLog( "Error while trying to log out : \(logOutError.localizedDescription)")
         }
       
-        
         let storyboard = UIStoryboard(name: "Start", bundle: nil)
       let SignInVC =   storyboard.instantiateViewController(withIdentifier: "SignInVC")
         self.present(SignInVC, animated: true, completion: nil)
